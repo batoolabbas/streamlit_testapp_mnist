c4169aa60dfc125a3385eb60aabb42bc57dd16c4
Ubuntu <ubuntu@ip-172-31-40-104.us-west-2.compute.internal>
2022-08-23 06:12:01 +0000

    requirements file bug fix

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 @st.cache
-def get_MNIST(train=False):
+def get_MNIST(train=True):
     return MNIST(root='~/',train=train,download=True, transform=transforms.Compose([transforms.ToTensor()]))
 
 @st.cache
 def get_PCATSNE(b_size=64,subset=10000,pca_components=50):
     ds = get_MNIST()
+    print(len(ds))
     data_loader = torch.utils.data.DataLoader(ds, batch_size=b_size)
     img, label = next(iter(data_loader))
     img = img.view((b_size,-1))
@@ -27,15 +28,40 @@ def get_PCATSNE(b_size=64,subset=10000,pca_components=50):
     vis_data = pd.DataFrame(data={'x':pca_tsne[:,0],'y':pca_tsne[:,1],'label':subset_y})
     return vis_data
 
+@st.cache
+def get_TSNE(b_size=64,subset=10000):
+    ds = get_MNIST()
+    print(len(ds))
+    data_loader = torch.utils.data.DataLoader(ds, batch_size=b_size)
+    img, label = next(iter(data_loader))
+    img = img.view((b_size,-1))
+    label = label.view((-1))
+    subset_x = img[:subset]
+    subset_y = label[:subset]
+    tsne = TSNE(random_state=42,n_components=2,verbose=True,perplexity=40, n_iter=300).fit_transform(subset_x)
+    print('fitted transform now creating df')
+    vis_data = pd.DataFrame(data={'x':tsne[:,0],'y':tsne[:,1],'label':subset_y})
+    return vis_data
+
+
 st.write('MNIST embedding visualization experiment')    
 b_size=128
-cn = None
+cn = 50
 
-vis_data = get_PCATSNE(b_size=b_size,subset=10000,pca_components=cn)
+vis_data = get_TSNE(b_size=b_size,subset=10000)
 
-fig = plt.gcf()
+fig = plt.figure()
 ax = fig.subplots()
 ch_plt = sns.scatterplot(data=vis_data,x="x",y="y",hue="label",ax=ax,palette='Spectral')
+ch_plt.set(title='TSNE without PCA')
+print('sending to streamlit')
+st.pyplot(fig, use_container_width=True)
+
+vis_data = get_PCATSNE(b_size=b_size,subset=10000,pca_components=cn)
+fig = plt.figure()
+ax = fig.subplots()
+ch_plt = sns.scatterplot(data=vis_data,x="x",y="y",hue="label",ax=ax,palette='Spectral')
+ch_plt.set(title='TSNE after PCA to 50 components')
 print('sending to streamlit')
 st.pyplot(fig, use_container_width=True)
 
