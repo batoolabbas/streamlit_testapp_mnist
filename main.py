a4495ef97c195411e6e2bd51acb5b1307961b323
Ubuntu <ubuntu@ip-172-31-40-104.us-west-2.compute.internal>
2022-08-23 04:59:50 +0000

    interactive bugfix

--- main.py
+++ main.py
@@ -13,14 +13,14 @@ def get_MNIST(train=False):
     return MNIST(root='~/',train=train,download=True, transform=transforms.Compose([transforms.ToTensor()]))
 
 @st.cache
-def get_PCATSNE(data_loader,b_size=64,subset=1000,pca_components=50):
+def get_PCATSNE(data_loader,b_size=64,subset=10000,pca_components=50):
     img, label = next(iter(data_loader))
     img = img.view((b_size,-1))
     label = label.view((-1))
     subset_x = img[:subset]
     subset_y = label[:subset]
     pca_k = PCA(n_components=pca_components).fit_transform(subset_x)
-    pca_tsne = TSNE(random_state=40,n_components=2,verbose=True).fit_transform(pca_k)
+    pca_tsne = TSNE(random_state=42,n_components=2,verbose=True,perplexity=40, n_iter=300).fit_transform(pca_k)
 
     vis_data = pd.DataFrame(data={'x':pca_tsne[:,0],'y':pca_tsne[:,1],'label':subset_y})
     return vis_data
@@ -34,7 +34,7 @@ def plot_tsne(b_size,cn):
     vis_data = get_PCATSNE(data_loader,b_size=b_size,pca_components=cn)
     fig = plt.gcf()
     ax = fig.subplots()
-    ch_plt = sns.scatterplot(data=vis_data,x="x",y="y",hue="label",ax=ax)
+    ch_plt = sns.scatterplot(data=vis_data,x="x",y="y",hue="label",ax=ax,palette='spectral')
     return fig
 
 b_size=st.sidebar.slider("Number of samples", min_value=1,max_value=10000,value=128)
